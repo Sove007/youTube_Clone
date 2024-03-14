521de26bd4c773ee7b60939a44e8b1c21f066487
Sove007 <dannykalifa0@gmail.com>
2024-03-14 13:12:35 +0530

    adding  hover effect on videocard

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -11,8 +11,8 @@ const VideoCard = ({ info }) => {
 
     const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
   return (
-    <div className="m-2  w-[317px] mb-7 hover:shadow-xl">
-      <img className="rounded-lg" src={thumbnails.medium.url} alt="banner" />
+    <div className="m-2  w-[317px] mb-7 hover:scale-y-105 transition-transform duration-300 ease-in-out">
+      <img className="rounded-lg hover:rounded-sm" src={thumbnails.medium.url} alt="banner" />
       <h1 className="font-semibold mt-2">{title}...</h1>
       <h4>{channelTitle} ✅</h4>
       <ul className="flex text-sm font-light">
